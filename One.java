c5f8c2afae2a604f3ac85c08440a7990a39c2924
rajeswaripullagura <rajipullagura99@gmail.com>
2020-07-12 17:36:38 +0530

    chech once

--- One.java
+++ One.java
@@ -4,33 +4,8 @@ import java.util.Scanner;
 
 public class Checkpolidrome {
 
-	public static void main(String[] args) {
-//		String reverse= "";
-//		System.out.println("enter your polindrome:");
-//		Scanner scanner = new Scanner(System.in);
-//		String string = scanner.next();
-//		for (int i = string.length() - 1; i >= 0; i--) {
-//			reverse = reverse + string.charAt(i);
-//
-//		}
-//		if (string.equals(reverse)) {
-//			System.out.println(reverse + "  is polindrome");
-//		} else {
-//			System.out.println("given string is not polindrome");
-//		}
-
-		String s = "radar";
-		int length = s.length();
-		String rev = "";
-		for (int i = length - 1; i >= 0; i--) {
-			rev = rev + s.charAt(i);
-		}
-		if(s.equals(rev)) {
-			System.out.println("polindrone");
-		}
-		else {
-			System.out.println("not polindrome");
-		}
+	public static void main(String[] args){		
+		
 	}
 
 }
